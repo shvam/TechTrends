d4aee7a97b68573893ed6594dfdae9996640af00
Shivam <shivam@Shivams-MacBook-Air.local>
2021-12-14 09:44:50 +0530

    logging

--- app.py
+++ app.py
@@ -3,11 +3,14 @@ import logging
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
 
+connection_count = 0
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
+    global connection_count
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+    connection_count += 1
     return connection
 
 # Function to get a post using its ID
@@ -42,9 +45,10 @@ def healthz():
 @app.route('/metrics')
 def metrics():
     connection = get_db_connection()
+    global connection_count
     posts = connection.execute('SELECT *FROM posts').fetchall()
     response=app.response_class(
-        response=json.dumps({"db_connection_count":1,"post_count":len(posts)}),
+        response=json.dumps({"db_connection_count":connection_count,"post_count":len(posts)}),
         status=200,
         mimetype='application/json' 
     )
@@ -87,5 +91,5 @@ def create():
 
 # start the application on port 3111
 if __name__ == "__main__":
-   logging.basicConfig(filename='app.log',level=logging.DEBUG)
+   logging.basicConfig(level=logging.DEBUG)
    app.run(host='0.0.0.0', port='3111')
